4bd6f583647f15639021943ba9aee33a20812a67
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-12-10 15:29:15 +0200

    11.6

--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -1,3 +1,4 @@
+/* global jest, describe, it, expect */
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import axiosMock from 'axios'
--- test/PokemonList.jest.spec.jsx
+++ test/PokemonList.jest.spec.jsx
@@ -1,3 +1,4 @@
+/* global describe, it, expect */
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
